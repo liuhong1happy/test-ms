e84ae7feec2fdafedd8d5f6a4892231cfa61f96d
Holly Liu <liuhong1.happy@163.com>
2026-04-22 14:45:32 +0800

    fix: 构建脚本错误

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,6 +15,6 @@ npm run build
 cd ..
 
 mkdir -p dist/zh dist/en
-cp chiness/dist/public/* dist/zh/
-cp english/dist/public/* dist/en/
+cp -r  chiness/dist/public/* dist/zh/
+cp -r  english/dist/public/* dist/en/
 cp index.html dist/
