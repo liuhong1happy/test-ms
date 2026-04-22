9a17057387279b1dc09a1c83198861f0b3a5d523
Holly Liu <liuhong1.happy@163.com>
2026-04-22 11:39:04 +0800

    feat: 更新构建脚本

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # 构建项目
+echo "构建中文项目"
 cd chiness
 npm install --legacy-peer-deps
 npm run build
 cd ..
 
 # 构建英文项目
+echo "构建英文项目"
 cd english
 npm install --legacy-peer-deps
 npm run build
 cd ..
+
+echo "构建Docker镜像 ${IMAGE_NAME}:${IMAGE_TAG}"
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
 
 echo "推送构建Docker镜像 ${IMAGE_NAME}:${IMAGE_TAG}"
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${DOCKER_REGISTRY_SERVER}/${IMAGE_NAME}:${IMAGE_TAG}
